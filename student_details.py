82e4b6880f5c5b92ffc9eb4bf46bd843fdcc41dc
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-01 14:53:44 +0530

    Add files via upload
    My first Python program

---
+++ student_details.py
@@ -0,0 +1,8 @@
+name=input("Enter the Name of the Student:")
+roll_no=int(input("Enter the Roll No:"))
+cgpa=float(input("Enter cgpa:"))
+percentage=cgpa*10
+print("Name of the Student:",name)
+print("Roll No:",roll_no)
+print("cgp of student:",cgpa)
+print("Percentage:",percentage,"%")
