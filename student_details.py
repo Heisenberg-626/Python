987cbcf0969bf1191ef6bc9559c31b1a46681dc6
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-01 15:21:10 +0530

    Add files via upload
    Document header added

--- student_details.py
+++ student_details.py
@@ -1,3 +1,10 @@
+'''
+Python program to get the student details
+Author: Sourav V S
+Date: 01-10-2024
+Version: 1.0
+'''
+
 name=input("Enter the Name of the Student:")
 roll_no=int(input("Enter the Roll No:"))
 cgpa=float(input("Enter cgpa:"))
