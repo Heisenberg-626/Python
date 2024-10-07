9d9d27e2c3d44595e9b631e5a1bde4dd42db443a
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-07 22:05:11 +0530

    Update math_module.py

--- math_module.py
+++ math_module.py
@@ -7,4 +7,4 @@ num1=int(input("Enter a Number:"))
 import math
 print("Square root of",num1,":",math.sqrt(num1))
 print("Factorial of",num1,":",math.factorial(num1))
-print(num1,"raised to power 2:",math.pow(5,2))
+print(num1,"raised to power 2:",math.pow(num1,2))
