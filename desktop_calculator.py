1288021e9d13cfef9e47137a6663ccb9f803f149
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-08 15:03:15 +0530

    Add files via upload

---
+++ desktop_calculator.py
@@ -0,0 +1,21 @@
+'''
+Author: Sourav V s
+Date: 08-10-2024
+This program is used to create a simple desktop calculator
+'''
+num1=int(input ("Enter first number:"))
+num2=int(input("Enter second number:"))
+num3=int(input("Enter third number:"))
+sum=num1+num2
+sub=num2-num1
+prod=num1*num2
+div=num1/num2
+rem=num1%num2
+result=(num1+num2)*num3/2
+print("The sum of num1 and num2 is:",sum)
+print("The difference when num2 is substracted from num1 is:",sub)
+print("The product of num1 and num2 is:",prod)
+print("The quotient when num1 is divided by num2 is:",div)
+print("The reminder when num1 is divided by num2 is:",rem)
+print("The result of (num1 + num2) * num3 / 2 is:",result)
+
