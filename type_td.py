3fba7670a547faeb8eefcabe268aa623cd110fbf
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-14 23:41:04 +0530

    Update type_td.py

--- type_td.py
+++ type_td.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from datetime import date
 current_time=datetime.now()
 print(current_time)
 format1=current_time.strftime("%Y-%m-%d %H:%M:%S")
@@ -12,12 +11,5 @@ format4=current_time.strftime("%A,%B %d,%Y %H:%M:%S %p")
 print(format4)
 format5=current_time.strftime("%a-%b-%d %H:%M:%S %Z %Y")
 print(format5)
-format6=current_time.strftime("%a-%b %d %H:%M:%S %Z %Y")
-print(format6)
-format7=current_time.strftime("%a-%b %d %H:%M:%S %Z %Y")
-var1=date.today()
-print(var1)
-var2=var1.isoformat()
-print(var2)
 
 
