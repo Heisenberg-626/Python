72d0affe3a2624d220d1e292398117bdc901e773
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 16:11:17 +0530

    Add files via upload

---
+++ odd_num_limit.py
@@ -0,0 +1,10 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to print odd numbers upto a limit
+'''
+limit=int(input("Enter the limit:"))
+odd_num=1
+while odd_num<=limit:
+    print(odd_num,"",end="")
+    odd_num+=2
