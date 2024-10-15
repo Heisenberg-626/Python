59b6f10a92484dd5d3dc4e83d10f9b0fc9852381
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 14:51:22 +0530

    Add files via upload

---
+++ colour_code.py
@@ -0,0 +1,14 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to print the colour based on a code value
+'''
+colour_code=input("Enter the colour code:")
+if colour_code=="R" or colour_code=="r":
+    print("The colour is: Red")
+elif colour_code=="G" or colour_code=="g":
+    print("The colour is: Green")
+elif colour_code=="B" or colour_code=="b":
+    print("The colour is: Blue")
+else:
+    print("Wrong code")
