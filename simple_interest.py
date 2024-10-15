4357662d388fb17ca0ad2d0b20d5ac48df1a6b59
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 14:18:58 +0530

    Add files via upload

---
+++ simple_interest.py
@@ -0,0 +1,10 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to find the simple interest
+'''
+principle_amount=float(input("Enter the principle amount:"))
+no_of_years=int(input("Enter the number of years:"))
+rate=float(input("Enter the rate:"))
+simple_interest=principle_amount*no_of_years*rate
+print("Simple interest:",simple_interest)
