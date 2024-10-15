1dacf88cfe474dcc83964c83182357dae60e0ea3
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 14:02:16 +0530

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,10 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to check whether the given number is positive or not
+'''
+num=int(input("Enter a number:"))
+if num>0:
+    print("The given number:",num,"is positive")
+else:
+    print("The given number:",num,"is negative")
