686dc5ba2d9377785ef030470d61a642d195c5f2
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 15:12:15 +0530

    Add files via upload

---
+++ driving_license_eligibility.py
@@ -0,0 +1,10 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to check whether if a person is eligible for license or not
+'''
+age=int(input("Enter the age:"))
+if age>=16:
+    print("Eligible for a driving license")
+else:
+    print("Not eligible for a driving license")
