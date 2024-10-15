4c25826272251b6dcb5cc8b5bfad9f29e9678b72
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 14:24:53 +0530

    Add files via upload

---
+++ larger_of_two_numbers.py
@@ -0,0 +1,11 @@
+'''
+Author: Sourav V S
+Date:08-10-2024
+This program is to determine the larger of two numbers
+'''
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+if num1>num2:
+    print("The larger number is:",num1)
+else:
+    print("The larger number is:",num2)
