96c4f68dfa51cca3aefd82bdcd40e5b170bc1c30
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-15 16:07:24 +0530

    Update print_with_limit.py

--- print_with_limit.py
+++ print_with_limit.py
@@ -1,7 +1,7 @@
 '''
 Author: Sourav V S
 Date:08-10-2024
-This program is to print odd numbers upto a certain limit
+This program is to print n odd numbers
 '''
 limit=int(input("Enter the limit:"))
 odd_num=1
