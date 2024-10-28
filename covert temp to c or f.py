20481de7e54fa503f0eccb2aaa5d2adf701660db
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-28 21:05:49 +0530

    Update covert temp to c or f.py

--- covert temp to c or f.py
+++ covert temp to c or f.py
@@ -3,13 +3,13 @@ Author: Sourav V S
 Date:22-10-2024
 This program is to convert temperature values back and forth between Celsius and Fahrenheit
 '''
-temp=float(input("Enter temperature:"))
+temperature=float(input("Enter temperature:"))
 unit=input("Is this in (C)elsius or (F)ahrenheit?")
 if unit=="F" or unit=="f":
-    c=5/9*(temp-42)
-    print(temp,"Fahrenheit is",c,"Celsius.")
+    c=5/9*(temperature)
+    print(temperature,"Fahrenheit is",c,"Celsius.")
 elif unit=="C" or unit=="c":
-    f=(9/5*temp)+32
-    print(temp,"Celsius is",f,"Fahrenheit.")
+    f=(9/5*temperature)+32
+    print(temperature,"Celsius is",f,"Fahrenheit.")
 else:
     print("Wrong input")
