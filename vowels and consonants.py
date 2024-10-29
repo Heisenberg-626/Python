9c249792c8de4c95d040cea079f963da3f3ebb9e
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-29 16:27:14 +0530

    Add files via upload

---
+++ vowels and consonants.py
@@ -0,0 +1,17 @@
+'''
+Author: Sourav V S
+Date: 22-10-2024
+This program is to find the number of vowels in a given string.
+'''
+str=input("Enter a string:")
+vowels="aeiouAEIOU"
+consonants="bcdfghjklmnpqrstvwxyz"
+vowels_count=0
+consonants_count=0
+for char in str:
+    if char in vowels:
+        vowels_count+=1
+    else:
+        consonants_count+=1
+print(f"No of vowels in the given string {str} : {vowels_count}")
+print(f"No of consonants in the given string{str}: {consonants_count}")
