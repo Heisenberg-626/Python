3bd2a2755edeadafc6506d869b77308827c14d14
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-10-29 15:23:41 +0530

    Update multiplication table.py

--- multiplication table.py
+++ multiplication table.py
@@ -1,3 +1,8 @@
+'''
+Author: Sourav V S
+Date: 22-10-2024
+This program is to make a sample multiplication table upto 12.
+'''
 number=int(input("Enter a number:"))
 for i in range(1,13):
     value=number*i
