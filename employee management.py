578565748402932ec4d9198aff5e08da6f091d11
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-11-19 16:28:11 +0530

    Add files via upload

---
+++ employee management.py
@@ -0,0 +1,12 @@
+emp_info = [("Ethan","Ethical Hacker",60000.0),
+            ("Zayn","Data Scientist",50000.0),
+            ("Neo","Web Developer",45000.0),
+            ("John Wick","Robotics",49000.0)]
+total_annual_expenses = 0
+threshold = float(input("Enter the threshold salary: "))
+for employee in emp_info:
+    name,department,salary = employee
+    total_annual_expenses += salary * 12
+    if salary > threshold:
+        print (f"Employees with monthly salary above threshold: {name}")
+print(f"Total annual payroll expenses for all employees by the company: {total_annual_expenses}")
