b3944d4e76c0b2e5e3703d7d4d6099c3869ceeb5
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-11-30 10:34:23 +0530

    Update mobile number or not.py

--- mobile number or not.py
+++ mobile number or not.py
@@ -1,3 +1,8 @@
+'''
+Author: Sourav V S
+Date: 30-11-2024
+This program to check whether the given number is a valid mobile number or not using functions.
+'''
 def mobile_no_or_not (mobile_num):
     digit = len(mobile)
     if digit == 10 and mobile[0]== "7" or mobile[0]== "8" or mobile[0]== "9":
