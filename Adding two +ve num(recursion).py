43f32e0f336b4c6612bccac997e2d3c392bf4e39
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-12-03 10:46:56 +0530

    Delete Adding two +ve num(recursion).py

--- Adding two +ve num(recursion).py
+++
@@ -1,8 +0,0 @@
-def add_num(n1,n2):
-    if n2==0:
-        return n1
-    else:
-        return add_num(n1+1,n2-1)
-num_1 = int(input("Enter the first number: "))
-num_2 = int(input("Enter the second number: "))
-print("Sum of two numbers: ",add_num(num_1,num_2))
