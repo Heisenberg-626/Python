0cd75cac59ad948cd9f4106c97fdb9cf5350a2fe
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-12-03 10:38:03 +0530

    Add files via upload

---
+++ Adding two +ve num(recursion).py
@@ -0,0 +1,8 @@
+def add_num(n1,n2):
+    if n2==0:
+        return n1
+    else:
+        return add_num(n1+1,n2-1)
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+print(add_num(num_1,num_2))
