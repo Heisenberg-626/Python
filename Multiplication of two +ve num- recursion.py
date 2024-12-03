aeeba7a8116db4f773b8ab37f1c97b487d50c683
Sourav-vs <souravvs2028@cs.sjcetpalai.ac.in>
2024-12-03 10:44:58 +0530

    Delete Multiplication of two +ve num- recursion.py

--- Multiplication of two +ve num- recursion.py
+++
@@ -1,8 +0,0 @@
-def multi_num(n1,n2):
-    if n2==1:
-        return n1
-    else:
-        return n1+multi_num(n1,n2-1)
-num_1 = int(input("Enter the first number: "))
-num_2 = int(input("Enter the second number: "))
-print(multi_num(num_1,num_2))
